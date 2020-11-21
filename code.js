5f96aea7061469daf8833a3600275280dbd126ed
Asbjørn Olav Orvedal <asbjorn.orvedal@gmail.com>
2020-11-21 15:13:21 +0100

    Ferdigstilte m/ 2 metodar. Restrukturerte.

--- code.js
+++ code.js
@@ -2,6 +2,7 @@
 
 let jaKnapp = document.getElementById("jaKnapp");
 const neiKnapp = document.getElementById("neiKnapp");
+const onsdskapTypeValg = document.getElementById("ondskapType");
 
 
 
@@ -9,6 +10,9 @@ const neiKnapp = document.getElementById("neiKnapp");
 
 function flyttElementTilfeldig(e, element) {
 
+    // Fjernar fokus frå jaKnapp dersom fokusert
+    if (document.activeElement === element) element.blur();
+    
     // Set høgaste moglege x- og y-verdiar for element
     // link https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
     const xMax = window.innerWidth - element.getBoundingClientRect().width;
@@ -18,28 +22,26 @@ function flyttElementTilfeldig(e, element) {
     const x = Math.floor(Math.random() * xMax);
     const y = Math.floor(Math.random() * yMax);
 
-    // Kopier element
-    jaKnapp = element.cloneNode(true);
+    // ? Kopier element berre dersom under <main>? elles berre endre style?
+    if (element.closest("main")) {
+        // Kopier element
+        jaKnapp = element.cloneNode(true);
 
-    // Set position og koordinater
-    jaKnapp.style.position = "absolute";
+        // Legg det nye elementet til i dokumentet og set position
+        document.body.appendChild(jaKnapp);
+        jaKnapp.style.position = "absolute";
+        
+        // Element i main-elementet må vere der for å halde ved strukturen
+        element.style.visibility = "hidden";
+        element.removeAttribute("id");
+    }
+
+    // Set nye koordinater
     jaKnapp.style.left = `${x}px`;
     jaKnapp.style.top = `${y}px`;
-
-    // Legg det nye elementet til i dokumentet
-    document.body.appendChild(jaKnapp);
-
-    // Dersom gamalt element ikkje er i main-elementet, slett det.
-    // link https://stackoverflow.com/questions/34292255/short-circuit-with-a-return-statement
-    // link https://www.w3schools.com/jsref/met_element_remove.asp
-    if (!element.closest("main")) return element.remove();
-
-    // Element i main-elementet må vere der for å halde ved strukturen
-    element.style.visibility = "hidden";
-    element.removeAttribute("id");
 }
 
-function vurderAvstand(e, element = jaKnapp, maalDiff = 150, reaksjonsFunksjon = flyttElementTilfeldig) {
+function vurderAvstand(e, element) {
 
     // Finn posisjonen til musepeikaren
     // link https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
@@ -61,16 +63,13 @@ function vurderAvstand(e, element = jaKnapp, maalDiff = 150, reaksjonsFunksjon =
     
     // Brukar Pythagoras for å finne avstand mellom musepeikar og element
     const diff = Math.sqrt((musX - elementXRef) ** 2 + (musY - elementYRef) ** 2);
-    if (diff < maalDiff && reaksjonsFunksjon) {
-        reaksjonsFunksjon(e, element);
-    }
 
     return diff;
 }
 
 function sorterDist(e, ...elementArr) {
     
-    // Lagar array med avstand frå musepeikar til kvart element i elementArr
+    // Lagar objekt med avstand frå musepeikar til kvart element i elementArr
     distArr = elementArr.map(element => {
         return {
             element: element,
@@ -81,24 +80,66 @@ function sorterDist(e, ...elementArr) {
     // Sorterer distArr
     distArr.sort((a, b) => a.dist - b.dist);
 
-    // Returnerer minste i distArr
     return distArr;
+}
+
+function skiftMetode(e, element = document.body, hendelseArr = [vurderFlyttKnapp, byttKnapper]) {
+
+    // Hentar den nye metoden frå <select>
+    const nyMetode = e.target.value;
+    
+    // Finn riktig hendele basert på nyMetode
+    // Må alltid stemme med element i hendelseArr
+    let nyHendelse;
+    switch (nyMetode) {
+        case "flytt":
+            // Set tilbake teksten på knappane
+            jaKnapp.innerHTML = "Ja!";
+            neiKnapp.innerHTML  = "Nei";
+            nyHendelse = vurderFlyttKnapp // ? endre funksjonsstruktur?
+            break;
+        case "bytt":
+            nyHendelse = byttKnapper
+            break;
+        default:
+            throw new Error("Denne ondskapen finst ikkje!");
+    }
+
+    // Fjernar alle noverane hendelsar
+    hendelseArr.forEach(hendelse => element.removeEventListener("mousemove", hendelse));
+    // Legg til riktig hendelse
+    element.addEventListener("mousemove", nyHendelse);
+}
+
+function byttKnapper(e) {
+    
+    // Set neiKnapp til knappen nærmast musepeikaren, jaKnapp motsett
+    const [nyNeiKnapp, nyJaKnapp] = sorterDist(e, jaKnapp, neiKnapp);
+    
+    // Set riktig tekst på knappane
+    nyNeiKnapp.element.innerHTML = "Nei";
+    nyJaKnapp.element.innerHTML = "Ja!";
+
+    // Ordnar med fokus via tastatur
+    jaKnapp.addEventListener("focus", e => jaKnapp.innerHTML = "Nei");
+    jaKnapp.addEventListener("focusout", e => jaKnapp.innerHTML = "Ja!");
+    neiKnapp.addEventListener("focus", e => neiKnapp.innerHTML = "Nei");
+    neiKnapp.addEventListener("focusout", e => neiKnapp.innerHTML = "Ja!");
+}
+
+function vurderFlyttKnapp(e, element = jaKnapp, maalDiff = 150, reaksjonsFunksjon = flyttElementTilfeldig) {
+    
+    const diff = vurderAvstand(e, element);
+    
+    // Forsøker berre å køyre dersom reaksjonsFunksjon er definert
+    if (diff < maalDiff && reaksjonsFunksjon) {
+        reaksjonsFunksjon(e, element);
+    }
 }
 
 
 
 // category event listeners
 
-// document.body.addEventListener("mousemove", vurderAvstand);
-document.body.addEventListener("mousemove", e => {
-    const [nyNeiKnapp, nyJaKnapp] = sorterDist(e, jaKnapp, neiKnapp);
-
-    nyNeiKnapp.element.innerHTML = "Nei";
-    nyJaKnapp.element.innerHTML = "Ja!";
-});
-
-// Ordnar med fokus via tastatur
-jaKnapp.addEventListener("focus", e => jaKnapp.innerHTML = "Nei");
-jaKnapp.addEventListener("focusout", e => jaKnapp.innerHTML = "Ja!");
-neiKnapp.addEventListener("focus", e => neiKnapp.innerHTML = "Nei");
-neiKnapp.addEventListener("focusout", e => neiKnapp.innerHTML = "Ja!");
+document.body.addEventListener("mousemove", vurderFlyttKnapp);
+onsdskapTypeValg.addEventListener("change", skiftMetode);
